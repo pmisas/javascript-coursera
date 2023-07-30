5ea10db6d2b42a6733771f2517cc7e59e6b357d6
Paula Misas <pmisas@unal.edu.co>
2023-07-29 23:52:48 -0500

    semana 1

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,20 @@
 /*alert("Hello Word")
 prompt("enter yor name: ")*/
 
-document.getElementById('test').innerHTML = "Time to learn javascript: ";
+//document.getElementById('test').innerHTML = "Time to learn javascript: ";
 //console.log("hola")
+
+
+
+//var name = prompt("whats your name?");
+document.write(name);
+console.log(name)
+
+var date = Date()
+document.write(date)
+
+var location = window.location;
+
+var width = window.innerWidth;
+document.write(location + width)
+alert(date)
